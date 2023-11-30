3ab4f6fec72329743e83d037a79608bf6fc19473
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-11-30 10:28:15 -0300

    Deixado de usar o a perm em memoria para usar agora em banco de dados

--- src/main/java/com/example/mapeamento/mapeamento/security/SecurityConfiguration.java
+++ src/main/java/com/example/mapeamento/mapeamento/security/SecurityConfiguration.java
@@ -1,15 +1,14 @@
 package com.example.mapeamento.mapeamento.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,7 +16,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration //classe de configuração
 @EnableWebSecurity //indica ao Spring que serão definidas configurações personalizadas de segurança
 public class SecurityConfiguration {
-
+    @Autowired
+    UsuarioDetailsConfig usuarioDetailsConfig;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
@@ -25,11 +25,11 @@ public class SecurityConfiguration {
                                 customizer
                                         .requestMatchers("/").permitAll()
                                         .requestMatchers("/home").permitAll()
-                                        .requestMatchers("/consultas/form").hasAnyRole("ADMIN","MEDICO")
-                                        .requestMatchers("/consultas/list").hasAnyRole("ADMIN","MEDICO")
-                                        .requestMatchers("/medicos/form").hasAnyRole("ADMIN","MEDICO")
-                                        .requestMatchers("/pacientes/form").hasAnyRole("ADMIN","MEDICO")
-                                        //.requestMatchers(HttpMethod.POST,"/consultas/save").permitAll()
+                                        .requestMatchers("/consultas/form").hasAnyRole("ERICK", "MEDICO")
+                                        .requestMatchers("/consultas/list").hasAnyRole("ERICK", "RECPCAO", "MEDICO")
+                                        .requestMatchers("/medicos/form").hasAnyRole("ERICK", "MEDICO")
+                                        .requestMatchers("/pacientes/form").hasAnyRole("ERICK", "RECPCAO")
+                                        .requestMatchers("/pacientes/list").hasAnyRole("ERICK", "RECPCAO", "MEDICO")
                                         .anyRequest() //define que a configuração é válida para qualquer requisição.
                                         .authenticated() //define que o usuário precisa estar autenticado.
                 )
@@ -45,28 +45,10 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user1 = User.withUsername("ana")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withUsername("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-        UserDetails erick = User.withUsername("erick")
-                .password(passwordEncoder().encode("erick"))
-                .roles("MEDICO")
-                .build();
-        return new InMemoryUserDetailsManager(user1, admin,erick);
+    @Autowired
+    public void configureUserDetails(final AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(usuarioDetailsConfig).passwordEncoder(new BCryptPasswordEncoder());
     }
-
-    /**
-     * Com o método, instanciamos uma instância do encoder BCrypt e deixando o controle dessa instância como responsabilidade do Spring.
-     * Agora, sempre que o Spring Security necessitar condificar um senha, ele já terá o que precisa configurado.
-     * @return
-     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
